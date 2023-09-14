126a41f324b3099114efd02855556be38dde0691
André Guilherme <130258901+AndreGuilhermeADS@users.noreply.github.com>
2023-09-14 00:16:48 -0300

    Localização e formatação de números e datas

--- SalesWebMVC/Startup.cs
+++ SalesWebMVC/Startup.cs
@@ -5,10 +5,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Localization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Globalization;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Data;
 using SalesWebMVC.Services;
@@ -53,6 +55,16 @@ namespace SalesWebMVC
         // Se colocarmos um parâmetro, e essa classe estiver registrada no sistema de injeção de dependência da aplicação, automaticamente será resolvido uma instância desse objeto.  
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+            
             if (env.IsDevelopment()) //Perfil de desenvolvimento
             {
                 app.UseDeveloperExceptionPage();
