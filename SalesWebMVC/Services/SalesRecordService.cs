b0cfee2e45eb45ed58d09e579bdaedce5fc8661c
Kaline Cecília Constantino <kaline.constantino@gmail.com>
2023-09-16 20:30:59 -0300

    Implementando busca agrupada

--- SalesWebMVC/Services/SalesRecordService.cs
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -17,7 +17,7 @@ namespace SalesWebMVC.Services
         }
 
         //Operação assíncrona que busca meus registros de vendas por data, sendo as datas minímas e máximas opcionais 
-        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)                                 //Na busca simples, ele retorna uma lista
         {
             var result = from obj in _context.SalesRecord select obj;
             if (minDate.HasValue)
@@ -29,9 +29,28 @@ namespace SalesWebMVC.Services
                 result = result.Where(x => x.Date <= maxDate.Value);
             }
             return await result
-                .Include(x => x.Seller)                 //Aqui estou fazendo os joins entre as tabelas 
+                .Include(x => x.Seller)                                                                                         //Aqui estou fazendo os joins entre as tabelas 
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate) //Aqui na busca agrupada, o retorno não será uma lista e sim um IGrouping
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)                  
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
                 .ToListAsync();
         }
     }
